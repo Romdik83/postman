0a2574ef2a0e4eba81c818bd728827bc8e7caac4
Romdik <romdik83@gmail.com>
2025-07-27 14:45:57 +0300

    postman test

--- README.md
+++
@@ -1,2 +0,0 @@
-# POSTMAN
-[![Build status](https://ci.appveyor.com/api/projects/status/5qhl08y9ere6e8im?svg=true)](https://ci.appveyor.com/project/Romdik83/postman)
---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+
+    @Test
+    void shouldReturnAccounts() {
+
+    // Given - When - Then
+    // Предусловия
+    given()
+    .baseUri("https://postman-echo.com")
+    .body("Hi")
+    // Выполняемые действия
+    .when().log().all()
+    .post("/post")
+   // Проверки
+    .then().log().all()
+    .statusCode(200)
+    .body("data", org.hamcrest.Matchers.equalTo("Hi"));
+} }
