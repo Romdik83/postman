2835f4105beb2713929d2347d5da0246900a07ae
Romdik <romdik83@gmail.com>
2025-07-27 14:50:44 +0300

    postman test

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -9,16 +9,17 @@ public class PostmanTest {
     @Test
     void shouldReturnAccounts() {
 
-    // Given - When - Then
-    // Предусловия
-    given()
-    .baseUri("https://postman-echo.com")
-    .body("Hi")
-    // Выполняемые действия
-    .when().log().all()
-    .post("/post")
-   // Проверки
-    .then().log().all()
-    .statusCode(200)
-    .body("data", org.hamcrest.Matchers.equalTo("Hi"));
-} }
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+                // Выполняемые действия
+                .when().log().all()
+                .post("/post")
+                // Проверки
+                .then().log().all()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("Hi"));
+    }
+}
